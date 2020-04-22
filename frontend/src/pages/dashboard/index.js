bb6f1880e9270ebfc8dd38213e05db246115bcaf
Filipe Melo <filipemelo032@gmail.com>
2020-04-22 18:10:27 -0300

    admin actions set

--- frontend/src/pages/dashboard/index.js
+++ frontend/src/pages/dashboard/index.js
@@ -1,77 +1,214 @@
-import React from "react";
+import React, { useState, useEffect, useMemo } from "react";
 
+import api from "../../services/api";
+
+import { FaEdit, FaTrash } from "react-icons/fa";
 import "./styles.css";
-export default function Dashboard() {
+export default function Dashboard({ history }) {
+  const [auth_id, setAuthId] = useState("");
+  const [img_url, setImgUrl] = useState("");
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+  const [itens, setItens] = useState([]);
+  const [stagedChange, setStagedChange] = useState({});
+  const [editBtn, setEditBtn] = useState("none");
+  const display = useMemo(() => (editBtn === "none" ? "block" : "none"));
+
+  async function onSubmit(e) {
+    e.preventDefault();
+    if (
+      img_url.length === 0 ||
+      title.length === 0 ||
+      description.length === 0 ||
+      price.length === 0
+    ) {
+      alert("Todos os campos são obrigatórios");
+      return;
+    }
+
+    try {
+      const response = await api.post(
+        "menu/item",
+        {
+          img_url,
+          title,
+          description,
+          price,
+        },
+        {
+          headers: {
+            auth_id,
+          },
+        }
+      );
+      setItens([...itens, response.data]);
+      setImgUrl("");
+      setTitle("");
+      setDescription("");
+      setPrice("");
+    } catch (err) {
+      alert("Erro ao salvar dados");
+    }
+  }
+
+  async function editClick(item, index) {
+    const { img_url, title, description, price, _id } = item;
+    setEditBtn("block");
+
+    setImgUrl(img_url);
+    setTitle(title);
+    setDescription(description);
+    setPrice(price);
+
+    setStagedChange({ id: _id, index });
+  }
+
+  async function logOut() {
+    await localStorage.clear();
+    history.push("/");
+  }
+  async function submitEdited(e) {
+    e.preventDefault();
+    if (
+      img_url.length === 0 ||
+      title.length === 0 ||
+      description.length === 0 ||
+      price.length === 0
+    ) {
+      alert("Todos os campos são obrigatórios");
+      return;
+    }
+
+    try {
+      const response = await api.put(
+        `menu/item/${stagedChange.id}`,
+        {
+          img_url,
+          title,
+          description,
+          price,
+        },
+        {
+          headers: {
+            auth_id,
+          },
+        }
+      );
+
+      const draft = itens;
+      draft[stagedChange.index] = {
+        img_url,
+        title,
+        description,
+        price,
+        _id: stagedChange.id,
+      };
+      setItens(draft);
+      setImgUrl("");
+      setTitle("");
+      setDescription("");
+      setPrice("");
+    } catch (err) {
+      alert("Erro ao atualizar dados");
+    }
+    setEditBtn("none");
+  }
+
+  async function submitDeleted(item) {
+    const draft = itens.filter((i) => i._id !== item._id);
+
+    try {
+      await api.delete(`menu/item/${item._id}`, {
+        headers: {
+          auth_id,
+        },
+      });
+      setItens(draft);
+    } catch (err) {
+      console.log("Erro ao deletar dado");
+    }
+  }
+  useEffect(() => {
+    async function logged() {
+      const id = await localStorage.getItem("auth_id");
+      if (!id) {
+        history.push("/");
+      } else {
+        setAuthId(id);
+        const menu = await api.get("menu/item");
+        setItens(menu.data);
+      }
+    }
+    logged();
+  }, [history]);
   return (
     <div className="dashboard">
       <div className="addForm">
-        <form method="post">
-          <input type="text" placeholder="Link da imagem" />
-          <input type="text" placeholder="Título" />
-          <textarea placeholder="Descrição"></textarea>
-          <input type="text" placeholder="Preço" />
-          <button>Adicionar ao Cardápio</button>
+        <form>
+          <input
+            type="text"
+            placeholder="Link da imagem"
+            value={img_url}
+            onChange={(e) => setImgUrl(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Título"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <textarea
+            placeholder="Descrição"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          ></textarea>
+          <input
+            type="text"
+            placeholder="Preço"
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+          />
+          <button onClick={onSubmit} style={{ display }}>
+            Adicionar ao Cardápio
+          </button>
+          <button onClick={submitEdited} style={{ display: editBtn }}>
+            Salvar alterações
+          </button>
+          <button onClick={() => logOut()} className="logOut">
+            Sair
+          </button>
         </form>
       </div>
       <div className="itensList">
         <ul>
-          <li>
-            <div className="cardItem">
-              <img
-                src="https://img.cybercook.com.br/receitas/845/coxinha-de-soja-1-623x350.jpeg"
-                alt="item"
-              />
-              <p>
-                <strong>Título exemplo</strong>
-              </p>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam
-              </p>
-              <span>
-                <strong>R$ 12,40</strong>
-              </span>
-            </div>
-          </li>
-          <li>
-            <div className="cardItem">
-              <img
-                src="https://img.cybercook.com.br/receitas/845/coxinha-de-soja-1-623x350.jpeg"
-                alt="item"
-              />
-              <p>
-                <strong>Título exemplo</strong>
-              </p>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam
-              </p>
-              <span>
-                <strong>R$ 12,40</strong>
-              </span>
-            </div>
-          </li>
-          <li>
-            <div className="cardItem">
-              <img
-                src="https://img.cybercook.com.br/receitas/845/coxinha-de-soja-1-623x350.jpeg"
-                alt="item"
-              />
-              <p>
-                <strong>Título exemplo</strong>
-              </p>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam
-              </p>
-              <span>
-                <strong>R$ 12,40</strong>
-              </span>
-            </div>
-          </li>
+          {itens.map((item, index) => (
+            <li key={item._id}>
+              <div className="cardItem">
+                <img src={item.img_url} alt="item" />
+                <p>
+                  <strong>{item.title}</strong>
+                </p>
+                <p>{item.description}</p>
+                <span>
+                  <strong>
+                    {Intl.NumberFormat("pt-BR", {
+                      style: "currency",
+                      currency: "BRL",
+                    }).format(item.price)}
+                  </strong>
+                </span>
+                <section>
+                  <button onClick={() => editClick(item, index)}>
+                    <FaEdit color="#3d3d3d" size={24} />
+                  </button>
+                  <button onClick={() => submitDeleted(item)}>
+                    <FaTrash color="#3d3d3d" size={24} />
+                  </button>
+                </section>
+              </div>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
--- frontend/src/pages/logon/index.js
+++ frontend/src/pages/logon/index.js
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import Logo from "../assets/logo.svg";
 import "./styles.css";
+import api from "../../services/api";
 export default function Logon({ history }) {
-  function onSubmit(e) {
+  const [user, setUser] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function onSubmit(e) {
     e.preventDefault();
-    history.push("/dashboard");
+
+    try {
+      const response = await api.post("sessions", { user, password });
+      if (response.data) {
+        const { _id: id } = response.data;
+        localStorage.setItem("auth_id", id);
+        history.push("/dashboard");
+      }
+    } catch (err) {
+      alert("Usuário ou senha inválidos");
+    }
   }
+
+  useEffect(() => {
+    async function logged() {
+      const auth_id = await localStorage.getItem("auth_id");
+      if (auth_id) {
+        history.push("/dashboard");
+      }
+    }
+    logged();
+  }, [history]);
+
   return (
     <div className="container">
       <img src={Logo} height="250" alt="logo" />
@@ -14,8 +39,18 @@ export default function Logon({ history }) {
       <div className="formFields" onSubmit={onSubmit}>
         <h1>Faça Login</h1>
         <form mehtod="post">
-          <input type="text" placeholder="Usuário" />
-          <input type="password" placeholder="Senha" />
+          <input
+            type="text"
+            placeholder="Usuário"
+            value={user}
+            onChange={(e) => setUser(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Senha"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
           <button type="submit">Entrar</button>
         </form>
       </div>
