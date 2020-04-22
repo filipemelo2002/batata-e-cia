5120b8bfb2472d1a4d0f0017ac2d097100dfbac8
Filipe Melo <filipemelo032@gmail.com>
2020-04-22 11:43:38 -0300

    celebrate validation

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,6 +4,9 @@ const routes = Router()
 
 const SessionsController = require('./controllers/SessionsController')
 const AdminController = require('./controllers/AdminController')
+const MenuController = require('./controllers/MenuController')
+const middleware = require('./middlewares/auth')
+const { Joi, Segments, celebrate } = require('celebrate')
 
 routes.get('/', (req, res) => {
   return res.json({ message: 'Testing' })
@@ -12,4 +15,28 @@ routes.get('/', (req, res) => {
 routes.post('/sessions', SessionsController.create)
 routes.post('/admins', AdminController.create)
 
+routes.get('/menu/item', MenuController.index)
+routes.post('/menu/item', middleware, celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    title: Joi.string().required(),
+    description: Joi.string().required(),
+    img_url: Joi.string().required(),
+    price: Joi.number().required()
+  })
+}), MenuController.create)
+
+routes.put('/menu/item/:id', middleware, celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    title: Joi.string().required(),
+    description: Joi.string().required(),
+    img_url: Joi.string().required(),
+    price: Joi.number().required()
+  })
+}), MenuController.update)
+
+routes.delete('/menu/item/:id', middleware, celebrate({
+  [Segments.PARAMS]: Joi.object().keys({
+    id: Joi.string().required()
+  })
+}), MenuController.delete)
 module.exports = routes
