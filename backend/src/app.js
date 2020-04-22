85fa49b50d6dbac91bd07b8b501bce1d5f09cf7b
Filipe Melo <filipemelo032@gmail.com>
2020-04-22 11:43:56 -0300

    Erros treatment and mongoose config

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,15 +1,18 @@
 const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
+const { errors } = require('celebrate')
 const app = express()
 
 const routes = require('./routes')
 mongoose.connect('mongodb+srv://root:ovmRcXkZFda4Ptu4@cluster0-ipxq5.mongodb.net/batata-e-cia?retryWrites=true&w=majority', {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
+  useFindAndModify: false
 })
 app.use(cors())
 app.use(express.json())
 app.use(routes)
+app.use(errors())
 
 module.exports = app
