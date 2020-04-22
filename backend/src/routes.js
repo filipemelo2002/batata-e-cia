cf586c4c418b1211741b5ddd35af30bba1a6e6b7
Filipe Melo <filipemelo032@gmail.com>
2020-04-22 10:55:07 -0300

    Admin creation route

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,11 +3,13 @@ const { Router } = require('express')
 const routes = Router()
 
 const SessionsController = require('./controllers/SessionsController')
+const AdminController = require('./controllers/AdminController')
 
 routes.get('/', (req, res) => {
   return res.json({ message: 'Testing' })
 })
 
 routes.post('/sessions', SessionsController.create)
+routes.post('/admins', AdminController.create)
 
 module.exports = routes
