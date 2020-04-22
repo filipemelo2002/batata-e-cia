29fe908a21f2df547d911b6147a85f8f7687bb52
Filipe Melo <filipemelo032@gmail.com>
2020-04-22 10:55:34 -0300

    Sessions controller handling authentication

--- backend/src/controllers/SessionsController.js
+++ backend/src/controllers/SessionsController.js
@@ -1,6 +1,22 @@
+const Admin = require('../models/Admin')
+const bcrypt = require('bcryptjs')
 module.exports = {
 
   async create (req, res) {
-    return res.json({ message: 'Login route working' })
+    const { user, password } = req.body
+
+    if (!user || !password) return res.status(400).json({ message: 'missing parameters' })
+
+    const admin = await Admin.findOne({
+      user
+    })
+
+    if (!admin) return res.status(400).json({ message: 'user not found' })
+
+    const verifyPass = bcrypt.compareSync(password, admin.password_hash)
+
+    if (!verifyPass) return res.status(400).json({ message: 'wrong password' })
+
+    return res.json(admin)
   }
 }
